1f33eb12a8c29e3739ca76c3faf120cda11a136e
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-02-01 19:06:16 +0300

    add user profile page and dynamic route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Login from './pages/Login'
-
+import UserProfile from './pages/UserProfile'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />}></Route>
+                <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/profile/:id' element={<UserProfile />} />
             </Routes>
 
         </BrowserRouter>
