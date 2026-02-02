6156e35a515c88e232a4515ef83c39dbbab02e5d
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-02-02 19:24:43 +0300

    create outlet context of users and share with userCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,11 @@
-import { useState, useEffect } from "react"
+
 import UserCard from "../components/UserCard"
-import { Outlet } from "react-router-dom"
+import { Outlet, useOutletContext } from "react-router-dom"
 
 
 function Home() {
-  const [users, setUsers] = useState([])
+  const users = useOutletContext()
 
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error))
-  }, [])
-  
   const userList = users.map(user =>{
     return <UserCard key={user.id} user={user}/>
   })
